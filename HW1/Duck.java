72b7b9254ec21d98b03699020e7cd1f3a5f685cd
Diana <64835244+dbrodovikova@users.noreply.github.com>
2023-09-21 21:53:37 +0500

    Delete HW1 directory

--- HW1/Duck.java
+++
@@ -1,9 +0,0 @@
-public class Duck extends Animal{
-    public Duck (String name, String owner) {
-        super(name,owner);
-    }
-
-    public Duck(String name){
-        this(name,null);
-    }
-}
