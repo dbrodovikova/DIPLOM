c9f6d536b1b164925bc3290c4736f98767eebe7b
Diana <64835244+dbrodovikova@users.noreply.github.com>
2023-10-22 23:15:21 +0500

    Delete homework directory

--- homework/report/Reportable.java
+++
@@ -1,5 +0,0 @@
-package hw6.homework.report;
-
-public interface Reportable {
-    String report();
-}
--- homework/save/Savable.java
+++
@@ -1,5 +0,0 @@
-package hw6.homework.save;
-
-public interface Savable<T> {
-    boolean save(T t);
-}
