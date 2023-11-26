ba6ca4bfe37de84bae2d6c41ec0d7df8dcad7154
Diana <64835244+dbrodovikova@users.noreply.github.com>
2023-11-26 22:25:55 +0500

    Урок 3.
    Классы и объекты

---
+++ lesson3/src/main/java/ru/geekbrains/lesson3/Worker.java
@@ -0,0 +1,20 @@
+package ru.geekbrains.lesson3;
+
+abstract class Worker {
+    protected String name;
+
+    public Worker(String name) {
+        this.name = name;
+    }
+
+    /** Абстрактный метод расчёта среднемесячной заработной платы
+     *
+      */
+    public abstract double calculateAverageSalary();
+
+    public String getName() {
+        return name;
+    }
+}
+
+
