c34d840e8b65881bff5fbde907a3f3813db88896
Diana <64835244+dbrodovikova@users.noreply.github.com>
2024-06-01 14:12:17 +0500

    Delete HW12 directory

--- HW12/WebPageMicroService/src/main/java/ru/fsv67/repositories/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.fsv67.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.fsv67.models.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
